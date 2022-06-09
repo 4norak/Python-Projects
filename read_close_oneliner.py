1a87a392ac9895b1d82d9e4be0b1dc075f672d65
4norak <32487760+4norak@users.noreply.github.com>
2022-06-09 11:18:10 +0200

    Use *2 instead of itertools.tee

--- read_close_oneliner.py
+++ read_close_oneliner.py
@@ -1,2 +1,2 @@
 # Oneliner to open a file, read its content, close the file descriptor properly and return the content
-lambda file: tuple(map(lambda x: x[0](x[1]), zip((lambda x: next(x).read(), lambda x: next(x).close(),), __import__("itertools").tee((open(file, "r"),), 2))))[0]
+lambda file: tuple(map(lambda x: x[0](x[1]), zip((lambda x: x.read(), lambda x: x.close(),), (open(file, "r"),)*2)))[0]
